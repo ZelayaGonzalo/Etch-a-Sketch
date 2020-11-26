6bef9dc485acaebcb6b31d1d151065e627994f3d
ZelayaGonzalo <zelayagonzalo33@gmail.com>
2020-11-26 18:21:37 -0300

    Changed buttons and added erase function.

--- code.js
+++ code.js
@@ -1,4 +1,8 @@
+let numberofGrids=16;
 const container = document.getElementById("container");
+let color=0;
+let paintOnHover=false;
+let slider=document.getElementById("myRange");
 
 function makeGrid(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
@@ -8,16 +12,66 @@ function makeGrid(rows, cols) {
    // cell.innerText = (c + 1);
     container.appendChild(cell).className = "grid-item";
   }
-  var gridPixels=container.querySelectorAll('div');
+  let gridPixels=container.querySelectorAll('div');
   gridPixels.forEach(pixel=>pixel.addEventListener('mouseover',changeColorGrid));
+  gridPixels.forEach(pixel=>pixel.addEventListener('mousedown',start_stop_Painting));
 }
+
+function start_stop_Painting(){
+  paintOnHover=!paintOnHover;
+  
+
+}
+
+function erase(){
+  let gridPixels=container.querySelectorAll('div');
+  gridPixels.forEach(pixel=>pixel.style.backgroundColor="white");
+}
+
+function changeSizeofGrid(){
+  if(numberofGrids!=slider.value){
+    numberofGrids=slider.value;
+    erase();
+    makeGrid(numberofGrids,numberofGrids);
+  }
+ 
+
+}
+
+function randomColor(){
+  return '#'+Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
+}
+
+function changeColor(nColor){
+  color=nColor;
+  paintOnHover=false;
+  return;
+}
+
 function changeColorGrid(){
-    this.style.backgroundColor='#000000';
-    this.classList.remove('grid-item');
+  if(paintOnHover==true){
+    switch(color){
+      case 0:
+        this.style.backgroundColor='#000000';
+        this.classList.remove('grid-item');
+        break;
+      case 1:
+        this.style.backgroundColor='#0000FF';
+        this.classList.remove('grid-item');
+        break;
+      case 2:
+        this.style.backgroundColor=randomColor();
+        this.classList.remove('grid-item');
+        break;  
+      }
+ 
+  }
+  
+    
 }
   
     
 
-makeGrid(16, 16);
+makeGrid(numberofGrids,numberofGrids);
 
 
